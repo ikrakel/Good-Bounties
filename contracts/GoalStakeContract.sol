49b134c7b281e38c810c0fa173f0d77be17d1ff2
ikrakel <ikrakel@hey.com>
2023-07-21 16:37:56 +0100

    Add centralized function for retrieving funds of goals that have been abandoned

--- contracts/GoalStakeContract.sol
+++ contracts/GoalStakeContract.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 
 /*
 * The GoalStakeContract is designed to facilitate staking on NFT goals. Each NFT
@@ -17,13 +18,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 * are stubbed out and should be completed according to the specific ERC721 contract implementation.
 */
 
-contract GoalStakeContract {
+contract GoalStakeContract is AccessControl {
   address public goalNFT;
   // We're mapping a tokenId to another mapping that links an address to its stake
-  mapping(uint => mapping(address => uint)) public stakes;
+
+  struct StakeStruct {
+    mapping(address => uint) stakes;
+    bool terminated;
+  }
+
+  mapping(uint => StakeStruct) public stakes;
+  mapping(uint => uint) public totalStakesPerTokenId;
 
   constructor(address _goalNFT) {
     goalNFT = _goalNFT;
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
   }
 
   function stake(uint _tokenId) external payable {
@@ -32,21 +41,43 @@ contract GoalStakeContract {
     require(msg.value > 0, "Stake must be greater than 0");
 
     // Add the new stake to the mapping
-    stakes[_tokenId][msg.sender] += msg.value;
+    stakes[_tokenId].stakes[msg.sender] += msg.value;
+    totalStakesPerTokenId[_tokenId] += msg.value;
+  }
+
+  function getStake(uint _tokenId, address _address) external view returns (uint) {
+    if (stakes[_tokenId].terminated) return 0;
+
+    return stakes[_tokenId].stakes[_address];
   }
 
   function withdraw(uint _tokenId) external {
     // TODO: Add the require below once we have a collection ready
-    // require(ERC721(goalNFT).statusOf(_tokenId) == "complete", "Goal does not exist");
+    // require(ERC721(goalNFT).statusOf(_tokenId) == "complete", "Goal's state does not allow withdrawls");
 
-    uint stakeAmount = stakes[_tokenId][msg.sender];
+    uint stakeAmount = stakes[_tokenId].stakes[msg.sender];
     require(stakeAmount > 0, "No funds to withdraw");
+    require(stakes[_tokenId].terminated == false, "This goal already had its funds retrieved by an admin");
 
     // Reset the stake to 0
-    stakes[_tokenId][msg.sender] = 0;
+    stakes[_tokenId].stakes[msg.sender] = 0;
+    totalStakesPerTokenId[_tokenId] -= stakeAmount;
 
     // Transfer the stake back to the user
     payable(msg.sender).transfer(stakeAmount);
+  }
+
+  function adminWithdraw(uint _tokenId) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    // Assume that the total stake for a token can be calculated
+    uint totalStake = totalStakesPerTokenId[_tokenId];
+    require(totalStake > 0, "No funds to retrieve");
+
+    // Reset the stake mapping for the token
+    totalStakesPerTokenId[_tokenId] = 0;
+    stakes[_tokenId].terminated = true;
+
+    // Transfer the stake back to the admin
+    payable(msg.sender).transfer(totalStake);
   }
 }
 
--- test/contracts/GoalStakeContract.ts
+++ test/contracts/GoalStakeContract.ts
@@ -11,7 +11,7 @@ chai.use(solidity);
 
 const { expect } = chai;
 const { parseUnits } = ethers.utils;
-const { deployContract } = waffle;
+const { deployContract, provider } = waffle;
 
 describe("GoalStakeContract", () => {
   let creator: SignerWithAddress;
@@ -40,6 +40,10 @@ describe("GoalStakeContract", () => {
     ]) as Promise<GoalStakeContract>;
   };
 
+  const getBalanceOfStake = async (tokenId: number, address: string) => {
+    return await goalStakeContract.getStake(tokenId, address);
+  }
+
   describe("functions", () => {
     beforeEach(async () => {
       goalStakeContract = await builder();
@@ -49,7 +53,7 @@ describe("GoalStakeContract", () => {
       const tokenID = 1;
       await goalStakeContract.connect(addr1).stake(tokenID, { value: parseUnits("1") });
 
-      expect(await goalStakeContract.stakes(tokenID, addr1.address)).to.eq(parseUnits("1"));
+      expect(await getBalanceOfStake(tokenID, addr1.address)).to.eq(parseUnits("1"));
     });
 
     it("allows for more than 1 user to stake", async () => {
@@ -57,25 +61,45 @@ describe("GoalStakeContract", () => {
       await goalStakeContract.connect(addr1).stake(tokenID, { value: parseUnits("1") });
       await goalStakeContract.connect(addr2).stake(tokenID, { value: parseUnits("2") });
 
-      expect(await goalStakeContract.stakes(tokenID, addr1.address)).to.eq(parseUnits("1"));
-      expect(await goalStakeContract.stakes(tokenID, addr2.address)).to.eq(parseUnits("2"));
+      expect(await getBalanceOfStake(tokenID, addr1.address)).to.eq(parseUnits("1"));
+      expect(await getBalanceOfStake(tokenID, addr2.address)).to.eq(parseUnits("2"));
     });
 
     it("allows the users to withdraw their stake", async () => {
       const tokenID = 1;
       await goalStakeContract.connect(addr1).stake(tokenID, { value: parseUnits("1") });
-      expect(await goalStakeContract.stakes(tokenID, addr1.address)).to.eq(parseUnits("1"));
+      expect(await getBalanceOfStake(tokenID, addr1.address)).to.eq(parseUnits("1"));
 
       await goalStakeContract.connect(addr1).withdraw(tokenID);
-      expect(await goalStakeContract.stakes(tokenID, addr1.address)).to.eq(parseUnits("0"));
+      expect(await getBalanceOfStake(tokenID, addr1.address)).to.eq(parseUnits("0"));
     });
 
     it("does not allow anyone but the user to withdraw the stake", async () => {
       const tokenID = 1;
       await goalStakeContract.connect(addr1).stake(tokenID, { value: parseUnits("1") });
-      expect(await goalStakeContract.stakes(tokenID, addr1.address)).to.eq(parseUnits("1"));
+      expect(await getBalanceOfStake(tokenID, addr1.address)).to.eq(parseUnits("1"));
 
       expect(goalStakeContract.connect(addr2).withdraw(tokenID)).to.be.revertedWith('No funds to withdraw');
+    });
+
+    it("allows an admin to withdraw all the stake", async () => {
+      const tokenID = 1;
+      await goalStakeContract.connect(addr1).stake(tokenID, { value: parseUnits("1") });
+      await goalStakeContract.connect(addr2).stake(tokenID, { value: parseUnits("2") });
+
+      const balanceBefore = await provider.getBalance(creator.address);
+      await goalStakeContract.connect(creator).adminWithdraw(tokenID);
+      const balanceAfter = await provider.getBalance(creator.address);
+
+      expect(balanceAfter.sub(balanceBefore)).to.be.closeTo(parseUnits("3"), parseUnits("0.001"));
+    });
+
+    it("does not allow a user to withdraw after an admin did", async () => {
+      const tokenID = 1;
+      await goalStakeContract.connect(addr1).stake(tokenID, { value: parseUnits("1") });
+      
+      await goalStakeContract.connect(creator).adminWithdraw(tokenID);
+      expect(goalStakeContract.connect(addr1).withdraw(tokenID)).to.be.revertedWith('This goal already had its funds retrieved by an admin');
     });
   });
 });
