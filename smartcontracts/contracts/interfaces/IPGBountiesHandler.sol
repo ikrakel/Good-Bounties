24d85b2b3a06c1e151a28f0386fdcb73a6a6c44a
AV <artem.bv@pm.me>
2023-07-22 09:46:39 +0200

    feat: update bounties interface

--- smartcontracts/contracts/interfaces/IPGBountiesHandler.sol
+++ smartcontracts/contracts/interfaces/IPGBountiesHandler.sol
@@ -12,11 +12,20 @@ enum PGBountyState {
 }
 
 interface IPGBountiesHandler {
-    function openBounty() external returns (uint256); // anyone (bounty owner)
-    function submitProof(uint256 _bountyId, string calldata _attestationHash) external; // anyone (contributor)
+    function openBounty() external payable returns (uint256); // anyone (bounty owner)
+
+    function submitProof(
+        uint256 _bountyId,
+        string calldata _attestationHash
+    ) external; // anyone (contributor)
+
     function validateProof(uint256 _bountyId) external; // onlyBountyOwner
+
     function denyProof(uint256 _bountyId) external; // onlyBountyOwner
+
     function disputeBounty(uint256 _bountyId) external; // onlyBountyContributor
+
     function claimBounty(uint256 _bountyId) external; // onlyStakingContract
+
     function expireBounty(uint256 _bountyId) external; // anyone
 }
