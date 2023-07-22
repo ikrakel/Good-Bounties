501aa94106859029950da6c899a081356c227fc7
ikrakel <6774691+ikrakel@users.noreply.github.com>
2023-07-22 08:01:35 +0100

    Remove rewards contract (#3)
    * Remove unused rewards pool contract
    
    * Add Event emitters
    
    ---------
    
    Co-authored-by: ikrakel <ikrakel@hey.com>

--- smartcontracts/contracts/BountyStakeContract.sol
+++ smartcontracts/contracts/BountyStakeContract.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import { IBountyRewardPool } from "./BountyRewardPool.sol";
 
 /*
 * The BountyStakeContract is designed to facilitate staking on NFT Bounties. Each NFT
@@ -31,15 +30,17 @@ interface IBountyStakeContract {
 
 contract BountyStakeContract is IBountyStakeContract {
   address public bountyNFT;
-  address public bountyRewardPool;
+
+  // Event declarations
+  event StakeMade(uint indexed tokenId, address indexed staker, uint amount);
+  event StakeWithdrawn(uint indexed tokenId, address indexed staker, uint amount);
 
   // We're mapping a tokenId to another mapping that links an address to its stake
   mapping(uint => mapping(address => uint)) public stakes;
   mapping(uint => uint) public totalStakesPerTokenId;
 
-  constructor(address _bountyNFT, address _bountyRewardPool) {
+  constructor(address _bountyNFT) {
     bountyNFT = _bountyNFT;
-    bountyRewardPool = _bountyRewardPool;
   }
 
   function getStake(uint _tokenId, address _address) external view  override(IBountyStakeContract) returns (uint) {
@@ -54,6 +55,8 @@ contract BountyStakeContract is IBountyStakeContract {
     // Add the new stake to the mapping
     stakes[_tokenId][msg.sender] += msg.value;
     totalStakesPerTokenId[_tokenId] += msg.value;
+
+    emit StakeMade(_tokenId, msg.sender, msg.value);
   }
 
   function withdraw(uint _tokenId) external override(IBountyStakeContract) {
@@ -63,6 +66,8 @@ contract BountyStakeContract is IBountyStakeContract {
     // TODO: Depending on the state of bounty it should be withdrawn for the staker or for the contributor
     // _withdrawForContributors(_tokenId, msg.sender);
     _withdrawForStakers(_tokenId, msg.sender);
+
+    emit StakeWithdrawn(_tokenId, msg.sender, stakeAmount);
   }
 
   function _withdrawForContributors(uint _tokenId, address _claimer) internal {
@@ -83,9 +88,6 @@ contract BountyStakeContract is IBountyStakeContract {
     totalStakesPerTokenId[_tokenId] -= stakeAmount;
 
     // Transfer the stake back to the user
-    // TODO: Add a lookup to the amount that we should split between the user and the pool
-    uint amountToWithdraw = stakeAmount / 2;
-    IBountyRewardPool(bountyRewardPool).contribute{value: amountToWithdraw}();
-    payable(_claimer).transfer(amountToWithdraw);
+    payable(_claimer).transfer(stakeAmount);
   }
 }
--- smartcontracts/test/contracts/BountyStakeContract.ts
+++ smartcontracts/test/contracts/BountyStakeContract.ts
@@ -4,7 +4,7 @@ import { solidity } from "ethereum-waffle";
 
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-import type { BountyStakeContract, BountyRewardPool } from "../../typechain-types";
+import type { BountyStakeContract } from "../../typechain-types";
 import { Artifacts } from "../shared";
 
 chai.use(solidity);
@@ -19,7 +19,6 @@ describe("BountyStakeContract", () => {
   let addr2: SignerWithAddress;
 
   let bountyStakeContract: BountyStakeContract;
-  let bountyRewardContract: BountyRewardPool;
 
   beforeEach(async () => {
     [creator, addr1, addr2] = await ethers.getSigners();
@@ -27,10 +26,8 @@ describe("BountyStakeContract", () => {
 
   it("can be deployed", async () => {
     // TODO: Add an actual ERC721 deploy here and replace the address below
-    bountyRewardContract = await deployContract(creator, Artifacts.BountyRewardPool, ["0x33041027dd8F4dC82B6e825FB37ADf8f15d44053"]);
     const action = deployContract(creator, Artifacts.BountyStakeContract, [
-      "0x33041027dd8F4dC82B6e825FB37ADf8f15d44053",
-      bountyRewardContract.address
+      "0x33041027dd8F4dC82B6e825FB37ADf8f15d44053"
     ]);
 
     await expect(action).not.to.be.reverted;
@@ -39,8 +36,7 @@ describe("BountyStakeContract", () => {
   const builder = async () => {
     // TODO: Add an actual ERC721 deploy here and replace the address below
     return deployContract(creator, Artifacts.BountyStakeContract, [
-      "0x33041027dd8F4dC82B6e825FB37ADf8f15d44053",
-      bountyRewardContract.address
+      "0x33041027dd8F4dC82B6e825FB37ADf8f15d44053"
     ]) as Promise<BountyStakeContract>;
   };
 
@@ -50,7 +46,6 @@ describe("BountyStakeContract", () => {
 
   describe("functions", () => {
     beforeEach(async () => {
-      bountyRewardContract = await deployContract(creator, Artifacts.BountyRewardPool, ["0x33041027dd8F4dC82B6e825FB37ADf8f15d44053"]);
       bountyStakeContract = await builder();
     });
 
