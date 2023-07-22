2a7f9250cfec0c93587e1ed2490758ab2d62a4aa
taesuklee <taesuk.lee.data@tuta.io>
2023-07-22 13:22:26 +0200

    claimBounty

--- smartcontracts/contracts/BountyContract.sol
+++ smartcontracts/contracts/BountyContract.sol
@@ -1,205 +1,210 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
-import "hardhat/console.sol";
-import {PGBountyState, IPGBountiesHandler} from "./interfaces/IPGBountiesHandler.sol";
-import {DelegatedAttestationRequest, EASCustom} from "./eas/EASCustom.sol";
+import 'hardhat/console.sol';
+import { PGBountyState, IPGBountiesHandler } from './interfaces/IPGBountiesHandler.sol';
+import { DelegatedAttestationRequest, EASCustom } from './eas/EASCustom.sol';
 
 contract BountyContract is ERC721URIStorage {
-    error BountyDeadlineBeforeNow();
-    error BountyDoesntExist();
-    error BountyIsNotOpened();
-    error BountyIsNotSubmitted();
-    error BountyIsNotVerified();
-    error BountyHasExpired();
-    error VerificationPeriodExpired();
-    error OnlyOwnerCanValidateAttestation();
+  error BountyDeadlineBeforeNow();
+  error BountyDoesntExist();
+  error BountyIsNotOpened();
+  error BountyIsNotSubmitted();
+  error BountyIsNotVerified();
+  error BountyHasExpired();
+  error VerificationPeriodExpired();
+  error OnlyOwnerCanValidateAttestation();
 
-    struct Bounty {
-        uint256 tokenId;
-        uint256 submissionDeadline;
-        uint256 verificationPeriod;
-        uint256 submittedTime;
-        address payable owner;
-        address payable contributor;
-        string attestationHash;
-        PGBountyState state;
-    }
-
-    event BountyCreated(
-        uint256 indexed tokenId,
-        address indexed owner,
-        PGBountyState state,
-        uint256 submissionDeadline,
-        uint256 verificationPeriod
-    );
-
-    event ProofSubmitted(
-        uint256 indexed tokenId,
-        address indexed contributor,
-        PGBountyState state,
-        string attestationHash
-    );
-
-    event ProofValidated(
-        uint256 indexed tokenId,
-        address indexed contributor,
-        PGBountyState state
-    );
-
-    event ProofDenied(
-        uint256 indexed tokenId,
-        address indexed contributor,
-        PGBountyState state
-    );
-
-    using Counters for Counters.Counter;
-
-    Counters.Counter private tokenIds;
+  struct Bounty {
+    uint256 tokenId;
+    uint256 submissionDeadline;
+    uint256 verificationPeriod;
+    uint256 submittedTime;
     address payable owner;
-    EASCustom eas;
-    mapping(uint256 => Bounty) private idToBounties;
+    address payable contributor;
+    string attestationHash;
+    PGBountyState state;
+  }
 
-    constructor() ERC721("Token", "NFT") {
-        owner = payable(msg.sender);
-        eas = new EASCustom();
+  event BountyCreated(
+    uint256 indexed tokenId,
+    address indexed owner,
+    PGBountyState state,
+    uint256 submissionDeadline,
+    uint256 verificationPeriod
+  );
+
+  event ProofSubmitted(
+    uint256 indexed tokenId,
+    address indexed contributor,
+    PGBountyState state,
+    string attestationHash
+  );
+
+  event ProofValidated(
+    uint256 indexed tokenId,
+    address indexed contributor,
+    PGBountyState state
+  );
+
+  event ProofDenied(
+    uint256 indexed tokenId,
+    address indexed contributor,
+    PGBountyState state
+  );
+
+  using Counters for Counters.Counter;
+
+  Counters.Counter private tokenIds;
+  address payable owner;
+  EASCustom eas;
+  mapping(uint256 => Bounty) private idToBounties;
+
+  constructor() ERC721('Token', 'NFT') {
+    owner = payable(msg.sender);
+    eas = new EASCustom();
+  }
+
+  function openBounty(
+    uint256 submissionDeadline,
+    uint256 verificationPeriod,
+    string memory uri
+  ) external payable returns (uint256) {
+    if (submissionDeadline < block.timestamp) revert BountyDeadlineBeforeNow();
+
+    tokenIds.increment();
+
+    uint256 newTokenId = tokenIds.current();
+
+    _safeMint(msg.sender, newTokenId);
+    _setTokenURI(newTokenId, uri);
+
+    createBounty(newTokenId, submissionDeadline, verificationPeriod);
+
+    return newTokenId;
+  }
+
+  function submitProof(
+    uint256 _bountyId,
+    string calldata _attestationHash
+  ) external {
+    Bounty storage bounty = idToBounties[_bountyId];
+    if (bounty.owner == address(0)) revert BountyDoesntExist();
+    if (bounty.state != PGBountyState.OPEN) revert BountyIsNotOpened();
+    if (block.timestamp > bounty.submissionDeadline) revert BountyHasExpired();
+
+    bounty.attestationHash = _attestationHash;
+    bounty.state = PGBountyState.SUBMITTED;
+    bounty.contributor = payable(msg.sender);
+    bounty.submittedTime = block.timestamp;
+
+    emit ProofSubmitted(
+      _bountyId,
+      msg.sender,
+      PGBountyState.SUBMITTED,
+      _attestationHash
+    );
+  }
+
+  function validateProof(
+    uint256 _bountyId,
+    DelegatedAttestationRequest calldata request
+  ) external {
+    _checksBeforeValidation(_bountyId);
+    eas.verifyAttest(request);
+
+    Bounty storage bounty = idToBounties[_bountyId];
+    bounty.state = PGBountyState.VALIDATED;
+
+    emit ProofValidated(_bountyId, bounty.contributor, PGBountyState.VALIDATED);
+  }
+
+  function denyProof(uint256 _bountyId) external {
+    _checksBeforeValidation(_bountyId);
+
+    Bounty storage bounty = idToBounties[_bountyId];
+    if (block.timestamp >= bounty.submissionDeadline) {
+      bounty.state = PGBountyState.EXPIRED;
+    } else {
+      bounty.state = PGBountyState.OPEN;
     }
 
-    function openBounty(
-        uint256 submissionDeadline,
-        uint256 verificationPeriod,
-        string memory uri
-    ) external payable returns (uint256) {
-        if (submissionDeadline < block.timestamp)
-            revert BountyDeadlineBeforeNow();
+    emit ProofDenied(_bountyId, bounty.contributor, bounty.state);
+  }
 
-        tokenIds.increment();
+  function _checksBeforeValidation(uint256 _bountyId) internal view {
+    Bounty memory bounty = idToBounties[_bountyId];
+    if (bounty.owner == address(0)) revert BountyDoesntExist();
+    if (bounty.owner != msg.sender) revert OnlyOwnerCanValidateAttestation();
+    if (bounty.state != PGBountyState.SUBMITTED) revert BountyIsNotSubmitted();
+    if (block.timestamp > bounty.submittedTime + bounty.verificationPeriod)
+      revert VerificationPeriodExpired();
+  }
 
-        uint256 newTokenId = tokenIds.current();
+  function createBounty(
+    uint256 tokenId,
+    uint256 submissionDeadline,
+    uint256 verificationPeriod
+  ) private {
+    idToBounties[tokenId] = Bounty(
+      tokenId,
+      submissionDeadline,
+      verificationPeriod,
+      0,
+      payable(msg.sender),
+      payable(address(0)),
+      '',
+      PGBountyState.OPEN
+    );
 
-        _safeMint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, uri);
+    emit BountyCreated(
+      tokenId,
+      owner,
+      PGBountyState.OPEN,
+      submissionDeadline,
+      verificationPeriod
+    );
+  }
 
-        createBounty(newTokenId, submissionDeadline, verificationPeriod);
+  function fetchBounties() external view returns (Bounty[] memory) {
+    uint256 itemCount = tokenIds.current();
+    uint256 currentIndex = 0;
 
-        return newTokenId;
+    Bounty[] memory items;
+
+    for (uint256 i = 0; i < itemCount; i++) {
+      if (idToBounties[i + 1].state == PGBountyState.OPEN) {
+        uint256 currentId = i + 1;
+
+        Bounty storage currentItem = idToBounties[currentId];
+        items[currentIndex] = currentItem;
+        currentIndex += 1;
+      }
     }
 
-    function submitProof(
-        uint256 _bountyId,
-        string calldata _attestationHash
-    ) external {
-        Bounty storage bounty = idToBounties[_bountyId];
-        if (bounty.owner == address(0)) revert BountyDoesntExist();
-        if (bounty.state != PGBountyState.OPEN) revert BountyIsNotOpened();
-        if (block.timestamp > bounty.submissionDeadline)
-            revert BountyHasExpired();
+    return items;
+  }
 
-        bounty.attestationHash = _attestationHash;
-        bounty.state = PGBountyState.SUBMITTED;
-        bounty.contributor = payable(msg.sender);
-        bounty.submittedTime = block.timestamp;
+  function fetchBounty(uint256 _tokenId) external view returns (Bounty memory) {
+    return idToBounties[_tokenId];
+  }
 
-        emit ProofSubmitted(
-            _bountyId,
-            msg.sender,
-            PGBountyState.SUBMITTED,
-            _attestationHash
-        );
+  function claimBounty(uint256 _bountyId) external {
+    Bounty storage bounty = idToBounties[_bountyId];
+
+    if (bounty.owner == address(0)) revert BountyDoesntExist();
+    if (
+      bounty.state == PGBountyState.VALIDATED ||
+      (bounty.state == PGBountyState.SUBMITTED &&
+        block.timestamp > bounty.submittedTime + bounty.verificationPeriod)
+    ) {
+      // logic
+    } else {
+      // error
     }
-
-    function validateProof(
-        uint256 _bountyId,
-        DelegatedAttestationRequest calldata request
-    ) external {
-        _checksBeforeValidation(_bountyId);
-        eas.verifyAttest(request);
-
-        Bounty storage bounty = idToBounties[_bountyId];
-        bounty.state = PGBountyState.VALIDATED;
-
-        emit ProofValidated(
-            _bountyId,
-            bounty.contributor,
-            PGBountyState.VALIDATED
-        );
-    }
-
-    function denyProof(uint256 _bountyId) external {
-        _checksBeforeValidation(_bountyId);
-
-        Bounty storage bounty = idToBounties[_bountyId];
-        if (block.timestamp >= bounty.submissionDeadline) {
-            bounty.state = PGBountyState.EXPIRED;
-        } else {
-            bounty.state = PGBountyState.OPEN;
-        }
-
-        emit ProofDenied(_bountyId, bounty.contributor, bounty.state);
-    }
-
-    function _checksBeforeValidation(uint256 _bountyId) internal view {
-        Bounty memory bounty = idToBounties[_bountyId];
-        if (bounty.owner == address(0)) revert BountyDoesntExist();
-        if (bounty.owner != msg.sender)
-            revert OnlyOwnerCanValidateAttestation();
-        if (bounty.state != PGBountyState.SUBMITTED)
-            revert BountyIsNotSubmitted();
-        if (block.timestamp > bounty.submittedTime + bounty.verificationPeriod)
-            revert VerificationPeriodExpired();
-    }
-
-    function createBounty(
-        uint256 tokenId,
-        uint256 submissionDeadline,
-        uint256 verificationPeriod
-    ) private {
-        idToBounties[tokenId] = Bounty(
-            tokenId,
-            submissionDeadline,
-            verificationPeriod,
-            0,
-            payable(msg.sender),
-            payable(address(0)),
-            "",
-            PGBountyState.OPEN
-        );
-
-        emit BountyCreated(
-            tokenId,
-            owner,
-            PGBountyState.OPEN,
-            submissionDeadline,
-            verificationPeriod
-        );
-    }
-
-    function fetchBounties() external view returns (Bounty[] memory) {
-        uint256 itemCount = tokenIds.current();
-        uint256 currentIndex = 0;
-
-        Bounty[] memory items;
-
-        for (uint256 i = 0; i < itemCount; i++) {
-            if (idToBounties[i + 1].state == PGBountyState.OPEN) {
-                uint256 currentId = i + 1;
-
-                Bounty storage currentItem = idToBounties[currentId];
-                items[currentIndex] = currentItem;
-                currentIndex += 1;
-            }
-        }
-
-        return items;
-    }
-
-    function fetchBounty(
-        uint256 _tokenId
-    ) external view returns (Bounty memory) {
-        return idToBounties[_tokenId];
-    }
+  }
 }
